d854ac5c268661bed205fd1352786d2ddbcd24ae
Jessica Nguy <jnguy2014@my.fit.edu>
2018-10-25 13:03:07 -0400

    Preliminary Command-line reading

--- eval.adb
+++ eval.adb
@@ -9,9 +9,19 @@
 -- Course: CSE 4250, Fall 2018
 -- Project: Project #3, Expression Evaluation
 
-
+with Ada.Command_line; use Ada.Command_Line;
 with Ada.Text_IO; use Ada.Text_IO;
 procedure eval is
 begin
    Put_Line ("Hello WORLD!");
+   
+	for Arg in 1..Argument_Count loop
+   
+		// Process argument item.. idk.
+		// How to parse 1 character at a time?
+		
+		// Also, do all work/calls/pop/pushing here
+		
+	end loop;
+	   
 end eval;
