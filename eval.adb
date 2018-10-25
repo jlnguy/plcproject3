c84128c34a202aced7adb696dbeda30ff91b6038
Eric Pereira <ericmpereira02@gmail.com>
2018-10-25 11:17:24 -0400

    stuff

---
+++ eval.adb
@@ -0,0 +1,17 @@
+-- To run ada run:
+-- gcc -c <name>.adb
+-- gnatbind <name>
+-- gnatling <name>
+-- ./<name>.exe
+-- 
+-- Author: Eric Pereira: epereira2015@my.fit.edu 
+-- Author: Jessica Nguyen, Jnguyen2014@my.fit.edu
+-- Course: CSE 4250, Fall 2018
+-- Project: Project #2, Expression Evaluation
+
+
+with Ada.Text_IO; use Ada.Text_IO;
+procedure eval is
+begin
+   Put_Line ("Hello WORLD!");
+end eval;
