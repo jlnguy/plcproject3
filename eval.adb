82a3dd467c9d99c1ebb30b099c205ffd66bec4e7
Jessica Nguy <jnguy2014@my.fit.edu>
2018-10-25 11:23:55 -0400

    Last name fix

--- eval.adb
+++ eval.adb
@@ -5,7 +5,7 @@
 -- ./<name>.exe
 -- 
 -- Author: Eric Pereira: epereira2015@my.fit.edu 
--- Author: Jessica Nguyen, Jnguy2014@my.fit.edu
+-- Author: Jessica Nguy, Jnguy2014@my.fit.edu
 -- Course: CSE 4250, Fall 2018
 -- Project: Project #3, Expression Evaluation
 
